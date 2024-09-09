1f640ddb6dc42e788275e4d1734f921c98800779
bastianSalinas <bast.s.rojas@gmail.com>
2024-09-09 00:42:15 -0300

    agregando acciones para deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  site: 'https://bastianSalinas.github.io',
+  base: 'Portafolio'
 })
