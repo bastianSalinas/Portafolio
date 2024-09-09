43f059cbe4d6d60d90134235086c200299501a4a
bastianSalinas <bast.s.rojas@gmail.com>
2024-09-09 01:25:25 -0300

    se configura vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
+import vercel from "@astrojs/vercel/serverless"
 
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://bastianSalinas.github.io',
-  base: 'Portafolio'
+  output: 'server',
+  adapter: vercel(),
 })
